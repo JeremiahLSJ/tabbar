c9b5cd9312e2f8625418a58df7a588e3c4a83314
Jeremiah <lsj970811@yeah.net>
2022-06-27 18:18:58 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <MyHeader></MyHeader>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyHeader from '@/components/MyHeader.vue'
+import MyFooter from '@/components/MyFooter.vue'
+// import MyUserInfo from '@/views/MyUserInfo.vue'
+// import MyGoodsSearch from '@/views/MyGoodsSearch.vue'
+// import MyGoodsList from '@/views/MyGoodsList.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MyHeader,
+    MyFooter
+    // MyUserInfo,
+    // MyGoodsSearch,
+    // MyGoodsList
+  },
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList'
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch'
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo'
+        }
+      ],
+      componentName: 'MyGoodsList'
+    }
   }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  padding: 50px 0;
 }
 </style>
